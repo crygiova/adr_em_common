d5035712c613234650d488e1e5e732705b95f257
crygiova <christian.giovanelli@aalto.fi>
2016-05-16 15:24:50 +0300

    Comparators in UpdateMessageContent

--- src/main/java/fi/aalto/itia/adr_em_common/SimulationMessageFactory.java
+++ src/main/java/fi/aalto/itia/adr_em_common/SimulationMessageFactory.java
@@ -1,13 +1,21 @@
 package fi.aalto.itia.adr_em_common;
 
+import java.util.Random;
+
 public class SimulationMessageFactory {
 
-	
-	//TODO instead of empty probably would be better to put the first UpdateMessageContent
-	public static SimulationMessage getRegisterMessage(String sender,
+	// TODO instead of empty probably would be better to put the first
+	// UpdateMessageContent
+	public static SimulationMessage getEmptyRegisterMessage(String sender,
 			String receiver) {
 		return new SimulationMessage(sender, receiver,
 				ADR_EM_Common.REG_HEADER, ADR_EM_Common.EMPTY);
+	}
+
+	public static SimulationMessage getRegisterMessage(String sender,
+			String receiver, UpdateMessageContent umc) {
+		return new SimulationMessage(sender, receiver,
+				ADR_EM_Common.REG_HEADER, umc);
 	}
 
 	public static SimulationMessage getRegisterDeny(String sender,
@@ -27,5 +35,17 @@ public class SimulationMessageFactory {
 		return new SimulationMessage(sender, receiver,
 				ADR_EM_Common.STATUS_UPDATE_HEADER, content);
 	}
+
+	public static UpdateMessageContent generateUpdateMessage(
+			Double currentConsumption, Double possibleCut, Double timeCut,
+			Double possibleIncrease, Double timeIncrease, String sender) {
+		return new UpdateMessageContent(currentConsumption, possibleCut,
+				timeCut, possibleIncrease, timeIncrease, sender);
+	}
 	
+	public static UpdateMessageContent generateRandomUpdateMessage( String sender) {
+		Random rand = new Random();
+		return new UpdateMessageContent(10 + 90 * rand.nextDouble(), 10 * rand.nextDouble(),
+				60* 15 + 15 * rand.nextDouble(), 10 * rand.nextDouble(), 60* 15 + 15 * rand.nextDouble(), sender);
+	}
 }
--- src/main/java/fi/aalto/itia/adr_em_common/UpdateMessageContent.java
+++ src/main/java/fi/aalto/itia/adr_em_common/UpdateMessageContent.java
@@ -2,20 +2,35 @@ package fi.aalto.itia.adr_em_common;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
+import java.util.Comparator;
 
-public class UpdateMessageContent implements Serializable {
+public class UpdateMessageContent implements Serializable, Comparable<UpdateMessageContent> {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 8089216898281578784L;
 
-	private Double currentConsumption;
-	private Double possibleCut;
-	private Double timeCut;
-	private Double possibleIncrease;
-	private Double timeIncrease;
+	private Double currentConsumption;// in W
+	private Double possibleCut;// in W
+	private Double timeCut;// in sec
+	private Double possibleIncrease;// in W
+	private Double timeIncrease;// in sed
 	private final Timestamp timeOfMessage;
+	private final String consumerSender;
+
+	// Empty
+	public UpdateMessageContent(String sender) {
+		super();
+		this.currentConsumption = 0d;
+		this.possibleCut = 0d;
+		this.timeCut = 0d;
+		this.possibleIncrease = 0d;
+		this.timeIncrease = 0d;
+		this.consumerSender = sender;
+		// NOW
+		this.timeOfMessage = new Timestamp(System.currentTimeMillis());
+	}
 
 	/**
 	 * @param currentConsumption
@@ -25,17 +40,18 @@ public class UpdateMessageContent implements Serializable {
 	 * @param timeIncrease
 	 */
 	public UpdateMessageContent(Double currentConsumption, Double possibleCut,
-			Double timeCut, Double possibleIncrease, Double timeIncrease) {
+			Double timeCut, Double possibleIncrease, Double timeIncrease,
+			String sender) {
 		super();
 		this.currentConsumption = currentConsumption;
 		this.possibleCut = possibleCut;
 		this.timeCut = timeCut;
 		this.possibleIncrease = possibleIncrease;
 		this.timeIncrease = timeIncrease;
-		//NOW
+		this.consumerSender = sender;
+		// NOW
 		this.timeOfMessage = new Timestamp(System.currentTimeMillis());
 	}
-
 
 	/**
 	 * @param currentConsumption
@@ -47,7 +63,7 @@ public class UpdateMessageContent implements Serializable {
 	 */
 	public UpdateMessageContent(Double currentConsumption, Double possibleCut,
 			Double timeCut, Double possibleIncrease, Double timeIncrease,
-			Timestamp timeOfMessage) {
+			Timestamp timeOfMessage, String sender) {
 		super();
 		this.currentConsumption = currentConsumption;
 		this.possibleCut = possibleCut;
@@ -55,6 +71,7 @@ public class UpdateMessageContent implements Serializable {
 		this.possibleIncrease = possibleIncrease;
 		this.timeIncrease = timeIncrease;
 		this.timeOfMessage = timeOfMessage;
+		this.consumerSender = sender;
 	}
 
 	public Double getCurrentConsumption() {
@@ -96,9 +113,66 @@ public class UpdateMessageContent implements Serializable {
 	public void setTimeIncrease(Double timeIncrease) {
 		this.timeIncrease = timeIncrease;
 	}
-	
+
 	public Timestamp getTimeOfMessage() {
 		return timeOfMessage;
 	}
 
+	public String getConsumerSender() {
+		return consumerSender;
+	}
+
+	public int compareTo(UpdateMessageContent o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/** Comparators **/
+
+	public static Comparator<UpdateMessageContent> SortByTimeCutComparator = new Comparator<UpdateMessageContent>() {
+		// if same time cut => goes to power cut
+		public int compare(UpdateMessageContent o1, UpdateMessageContent o2) {
+			if (o1.getTimeCut() > o2.getTimeCut())
+				return 1;
+			if (o1.getTimeCut() < o2.getTimeCut())
+				return -1;
+			if (o1.getPossibleCut() > o2.getPossibleCut())
+				return 1;
+			if (o1.getPossibleCut() < o2.getPossibleCut())
+				return -1;
+			return 0;
+		}
+	};
+
+	public static Comparator<UpdateMessageContent> SortByTimeIncreaseComparator = new Comparator<UpdateMessageContent>() {
+		// order time increase first and then possible increase if the time is
+		// the same
+		public int compare(UpdateMessageContent o1, UpdateMessageContent o2) {
+			if (o1.getTimeIncrease() > o2.getTimeIncrease())
+				return 1;
+			if (o1.getTimeIncrease() < o2.getTimeIncrease())
+				return -1;
+			if (o1.getPossibleIncrease() > o2.getPossibleIncrease())
+				return 1;
+			if (o1.getPossibleIncrease() < o2.getPossibleIncrease())
+				return -1;
+			return 0;
+		}
+	};
+
+	public static Comparator<UpdateMessageContent> SortByConsumerSenderComparator = new Comparator<UpdateMessageContent>() {
+		// compare only by consumer's name
+		public int compare(UpdateMessageContent o1, UpdateMessageContent o2) {
+			return o1.getConsumerSender().compareTo(o2.getConsumerSender());
+		}
+	};
+
+	@Override
+	public String toString() {
+		return "UpdateMessageContent [currentConsumption=" + currentConsumption
+				+ ", possibleCut=" + possibleCut + ", timeCut=" + timeCut
+				+ ", possibleIncrease=" + possibleIncrease + ", timeIncrease="
+				+ timeIncrease + ", timeOfMessage=" + timeOfMessage
+				+ ", consumerSender=" + consumerSender + "]";
+	}
 }
