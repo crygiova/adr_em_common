f7a249b1dd79354dd4a3be81b8e42685bb812651
crygiova <christian.giovanelli@aalto.fi>
2016-12-13 10:48:21 +0200

    Update of MaxSizedArray

--- src/main/java/fi/aalto/itia/util/MaxSizedArray.java
+++ src/main/java/fi/aalto/itia/util/MaxSizedArray.java
@@ -6,13 +6,15 @@ import org.apache.commons.math3.stat.descriptive.moment.Mean;
 public class MaxSizedArray {
 
     private CircularFifoQueue<Double> buffer;
+    private double last = 0d;
 
     public MaxSizedArray(int size) {
 	buffer = new CircularFifoQueue<Double>(size);
     }
 
     public void addElement(double el) {
-	buffer.add(el);
+	last = el;
+	buffer.add(last);
     }
 
     public double getMean() {
@@ -21,6 +23,10 @@ public class MaxSizedArray {
 	    m.increment(d);
 	}
 	return m.getResult();
+    }
+
+    public double getLast() {
+	return last;
     }
 
 }
